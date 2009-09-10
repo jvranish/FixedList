f0658c73329b64eb0bc5f1fb8d2a6a48269cf9ef
Job <job@job.(none)>
2009-09-10 19:27:30 -0400

    added FixedList typeclass constraint on the second parameter to Cons, also updated documentation some

--- src/Data/FixedList.hs
+++ src/Data/FixedList.hs
@@ -5,12 +5,18 @@
 {- |
 A fixed length list library.
 
-The length of a list is encoded into it's type in a natural way. This allows you to do things like specify that two list parameters have the same type, which also forces them to have the same length. This can be a handy property.
-It's not as flexible as the standard haskell list, but the added type safety is sometimes worth it.
+The length of a list is encoded into it's type in a natural way. This allows
+you to do things like specify that two list parameters have the same type,
+which also forces them to have the same length. This can be a handy property.
+It's not as flexible as the standard haskell list, but the added type safety is
+sometimes worth it.
 
-The entire library is Haskell98 except for the 'Append' typeclass. (which could be easily removed if needed).
+The entire library is Haskell98 except for the 'Append' typeclass. (which could
+be easily removed if needed).
 
-Most of your usual list functions ('foldr', 'fmap', 'sum', 'sequence', etc..) are accessed via the 'Functor', 'Applicative', 'Foldable', and 'Traversable' instances.
+Most of your usual list functions ('foldr', 'fmap', 'sum', 'sequence', etc..)
+are accessed via the 'Functor', 'Applicative', 'Foldable', and 'Traversable'
+instances.
 
 The Equivalent of zipWith can be had via the Applicative instance:
 
@@ -18,13 +24,14 @@ The Equivalent of zipWith can be had via the Applicative instance:
 
 Also, 'sequenceA' transposes a FixedList of FixedLists.
 
-The monad instance is also interesting. 
-return fills the list with the given element (remember that list size is dependent on the type)
-You can think of bind as operating like this:
+The monad instance is also interesting.
+return fills the list with the given element (remember that list size is
+dependent on the type) You can think of bind as operating like this:
 
-> m >>= k = diagonal $ fmap k m 
+> m >>= k = diagonal $ fmap k m
 
-This takes the FixedList m and maps k accross it, (which must return a FixedList) which results in a FixedList of FixedLists the diagonal of which is returned.
+This takes the FixedList m and maps k accross it, (which must return a FixedList)
+which results in a FixedList of FixedLists the diagonal of which is returned.
 The actually implementation is more elegant, but works essentialy the same.
 
 This also means that 'join' gets the diagonal of a FixedList of FixedLists.
@@ -33,24 +40,33 @@ You can construct FixedLists like so:
 
 > t1 :: Cons (Cons (Cons Nil)) Integer
 > t1 = 1 :. 3 :. 5 :. Nil
-> 
+>
 > t2 :: Cons (Cons (Cons Nil)) Integer  -- type signature needed! and must be correct!
 > t2 = fromFoldable' [4, 1, 0]
-> 
+>
 > t3 :: Cons (Cons (Cons Nil)) (Cons (Cons (Cons Nil)) Integer)
 > t3 = t1 :. t1 :. t2 :. Nil
-> 
+>
 > -- get the sum of the diagonal of the transpose of t3
 > test :: Cons (Cons (Cons Nil)) Integer
 > test = sum $ join $ sequenceA $ t3
 
+If you want to restrict a type to be a 'FixedList', but don't want to specify the
+size of the list, use the 'FixedList' typeclass:
+
+> myFunction :: (FixedList t) => t -> Float
 
 On a side note...
-I think that if Haskell supported infinite types my 'Append' typeclass would only have one parameter and I wouldn't need all those nasty extensions:
+I think that if Haskell supported infinite types my 'Append' typeclass would
+only have one parameter and I wouldn't need all those nasty extensions.
 
-I think I could also implement direct, typesafe, versions of 'last', 'init', 'reverse' and 'length' that don't depend on 'Foldable'. *sigh*  Maybe Haskell will one day support such things.
+I think I could also implement direct, typesafe, versions of 'last', 'init', 'reverse'
+and 'length' that don't depend on 'Foldable'. *sigh*  Maybe Haskell will one day
+support such things.
 
-This library is hosted on github (click on the /Contents/ link above and you should see the Homepage link) so it should be very easy to forked it, patch it,  and send patches back to me.
+This library is hosted on github (click on the /Contents/ link above and you should
+see the Homepage link) so it should be very easy to forked it, patch it, and send
+patches back to me.
 
 -}
 module Data.FixedList ( Cons(..)
@@ -67,7 +83,7 @@ module Data.FixedList ( Cons(..)
                       ) where
 
 import Control.Applicative
-import Control.Monad hiding (sequence) --hidden for haddock
+import Control.Monad hiding (sequence) --hiding for haddock
 import Data.Foldable
 import Data.Traversable
 import Data.Monoid
@@ -76,29 +92,33 @@ import Data.Maybe
 import Prelude hiding (head, tail, (++), foldr, sum, sequence, reverse, length, last, init)
 import qualified Prelude
 
-data Cons f a = (:.) { head :: a, 
-                       tail :: (f a)
-         }  deriving (Eq, Ord)
+data (FixedList f) =>
+  Cons f a = (:.) {
+    head :: a,
+    tail :: (f a)
+  }  deriving (Eq, Ord)
+
 data Nil a = Nil
   deriving (Eq, Ord)
 
 infixr 5 :.
 infixr 5  ++
 
-instance (Foldable f, Show a) => Show (Cons f a) where
+instance (FixedList f, Show a) => Show (Cons f a) where
   show x = "|" Prelude.++ show (toList x) Prelude.++ "|"
 instance Show (Nil a) where
   show Nil = "|[]|"
 
--- | just a restriction to make the fromFoldable's in reverse, and init safe
-class (Applicative f, Traversable f, Foldable f, Monad f) => FixedList f
+-- | Just a restrictive typeclass. It makes sure ':.' only takes FixedLists as it's second parameter
+--  and makes sure the use of fromFoldable's in reverse, and init is safe.
+class (Applicative f, Traversable f, Monad f) => FixedList f
 instance (FixedList f) => FixedList (Cons f)
 instance FixedList Nil
 
 -- The only very bad ugly, everything else is haskell98
 class Append f g h | f g -> h, f h -> g where
   (++) :: f a -> g a -> h a
-instance (Append f b c) => Append (Cons f) b (Cons c) where
+instance (FixedList f, FixedList c, Append f b c) => Append (Cons f) b (Cons c) where
   (x :. xs) ++ ys = x :. (xs ++ ys)
 instance Append Nil a a where
   Nil ++ ys = ys
@@ -123,7 +143,8 @@ unit :: a -> Cons Nil a
 unit a = a :. Nil
 
 -- | Converts any Foldable to any Applicative Traversable.
---   However, this will only do what you want if 'pure' gives you the shape of structure you are expecting.
+--   However, this will only do what you want if 'pure' gives you the
+--   shape of structure you are expecting.
 fromFoldable :: (Foldable f, Applicative g, Traversable g) => f a -> Maybe (g a)
 fromFoldable t = sequenceA $ snd $ mapAccumL f (toList t) (pure undefined)
   where
@@ -134,20 +155,20 @@ fromFoldable t = sequenceA $ snd $ mapAccumL f (toList t) (pure undefined)
 fromFoldable' :: (Foldable f, Applicative g, Traversable g) => f a -> g a
 fromFoldable' a = fromJust $ fromFoldable a
 
-instance (Functor f) => Functor (Cons f) where
+instance (FixedList f) => Functor (Cons f) where
   fmap f (a :. b) = (f a) :. (fmap f b)
 
-instance (Foldable f) => Foldable (Cons f) where
+instance (FixedList f) => Foldable (Cons f) where
   foldMap f (a :. b) = f a `mappend` foldMap f b
 
-instance (Traversable f) => Traversable (Cons f) where
+instance (FixedList f) => Traversable (Cons f) where
   traverse f (a :. b) = (:.) <$> f a <*> traverse f b
 
-instance (Monad f) => Monad (Cons f) where
+instance (FixedList f) => Monad (Cons f) where
   return x = x :. (return x)
   (a :. b) >>= k = (head $ k a) :. (b >>= (tail . k))
 
-instance (Monad f, Applicative f) => Applicative (Cons f) where
+instance (FixedList f) => Applicative (Cons f) where
   pure = return
   (<*>) = ap
 
@@ -163,7 +184,8 @@ instance Traversable Nil where
 
 instance Monad Nil where
   return _  = Nil
-  Nil >>= _ = Nil -- should I define this as _ >>= _ = Nil ? wouldn't have the right behavior with bottom
+  Nil >>= _ = Nil -- should I define this as _ >>= _ = Nil ? wouldn't
+                  -- have the right behavior with bottom
 
 instance Applicative Nil where
   pure = return
